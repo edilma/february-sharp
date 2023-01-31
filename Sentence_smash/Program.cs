1cd1a7b60e34d446346e77ce9bf665c3032457a2
Edilma Riano <edilmaria@hotmail.com>
2023-01-31 07:12:07 -0500

    sentence smash

---
+++ Sentence_smash/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentence_smash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "hello", "amazing", "world" };
+            Console.WriteLine(Smash(words));
+            Console.ReadLine();
+
+        }
+        public static string Smash(string[] words)
+        {
+            return String.Join(" ", words);
+        }
+    }
+}
+   
+
+
